9fd7bc76b78b477d2d117b6abf4db71a529c7502
Adriana Fernando <adriana.drih@gmail.com>
2020-06-05 12:47:21 -0300

    função singIn e createUser

--- public/index.js
+++ public/index.js
@@ -2,21 +2,21 @@
 // import { home, registro } from './pages/home/main.js';
 import routes from './pages/home/routes.js';
 
-const main = document.querySelector('.form');
+const main = document.querySelector('.form')
 
-const renderPage = () => {
+const init = () => {
+  window.addEventListener('hashchange', () => {
+    renderPage();
+  })
+};
+
+const renderPage = () =>{
   main.innerHTML = '';
   const page = validateHash(window.location.hash);
   main.appendChild(routes[page]);
 };
 
-const init = () => {
-  window.addEventListener('hashchange', () => {
-    renderPage();
-  });
-};
-
-const validateHash = (hash) => (hash === '' ? 'home' : hash.replace('#', ''));
+const validateHash = (hash) => hash === '' ? 'home' : hash.replace('#', '')
 window.addEventListener('load', () => {
   renderPage();
   init();
