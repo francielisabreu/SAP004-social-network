4cdfb282c10e7733afe47cdc019fc31582686b63
francielisabreu <contato.theframboesa@gmail.com>
2020-06-08 16:14:24 -0300

    funcao do fire para o login com facebook

--- public/pages/home/data.js
+++ public/pages/home/data.js
@@ -1,34 +1,55 @@
-// Aqui serão exportadas as funções que irão ser usadas
-
-//export const greeting = name => `Oi ${name}! Que bom ver você aqui!`;
-
-export function signIn (emailLog, passLog) { firebase.auth().singInWithEmailAndPassword(emailLog,
-    passLog).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessege = error.message;
-    alert('Não é um email válido')
-    })
+export function signIn(emailLog, passLog) {
+  firebase
+    .auth()
+    .singInWithEmailAndPassword(emailLog, passLog)
+    .catch(function (error) {
+      let errorCode = error.code;
+      let errorMessege = error.message;
+      alert("Não é um email válido");
+    });
 }
 
-export function createUser (emailUser, passUser) {firebase.auth().createUserWithEmailAndPassword(emailUser,
-        passUser).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessege = error.message;
-        alert('Não é um email válido')
-        })
-    }
+export function createUser(emailUser, passUser) {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(emailUser, passUser)
+    .catch(function (error) {
+      let errorCode = error.code;
+      let errorMessege = error.message;
+      alert("Não é um email válido");
+    });
+}
 
-export function userGoogle () {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-    var token = result.credential.accessToken;
-    var user = result.user;
-    }).catch(function(error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
+export function userGoogle() {
+  let provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      let token = result.credential.accessToken;
+      let user = result.user;
     })
-}    
+    .catch(function (error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      let email = error.email;
+      let credential = error.credential;
+    });
+}
+
+export function userFacebook() {
+  let provider = new firebase.auth.FacebookAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      let token = result.credential.accessToken;
+      let user = result.user;
+    })
+    .catch(function (error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      let email = error.email;
+      let credential = error.credential;
+    });
+}
